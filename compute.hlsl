022d3406f1da2e579eabdd14a931ce7163077180
Andrey Istomin <istomin_andrey@outlook.com>
2020-07-22 19:01:25 +0300

    ready

--- compute.hlsl
+++ compute.hlsl
@@ -1,6 +1,5 @@
 #define THREAD_X 32
 #define THREAD_Y 32
-#define SHARED_1
 
 struct Particle
 {
@@ -19,78 +18,6 @@ cbuffer globals
 	float3 sunDir;
 	float reserved;
 };
-
-#ifdef NAIVE
-
-float Overlap(in Particle caster, in Particle receiver) {
-
-	float sunDistDiff = dot(caster.pos - receiver.pos, sunDir);
-
-	if (sunDistDiff < 0) {
-		return 0.0f;
-	}
-
-	float3 dirReceiver = sunDir * dot(sunDir, receiver.pos);
-	float3 dirCaster = sunDir * dot(sunDir, caster.pos);
-
-	float3 posReceiver = receiver.pos - dirReceiver;
-	float3 posCaster = caster.pos - dirCaster;
-
-	float dist = length(posReceiver - posCaster);
-
-	return caster.opacity * min(caster.radius * caster.radius / (receiver.radius * receiver.radius), 1.0f) * smoothstep(receiver.radius + caster.radius, abs(receiver.radius - caster.radius), dist);
-}
-
-
-[numthreads(THREAD_X, THREAD_Y, 1)]
-void csComputeSelfShadowing(uint tid : SV_GroupIndex)
-{
-	float result = 1.0f;
-	const int N = THREAD_X * THREAD_Y;
-
-	for (int i = tid + 1; i < N + tid; ++i) {
-		result *= 1.0f - Overlap(sbParticles[i % N], sbParticles[tid]);
-	}
-
-	sbShadows[tid] = result;
-}
-
-#elif defined SHARED
-
-float Overlap(in Particle caster, in Particle receiver) {
-
-	const float sunDistDiff = dot(caster.pos - receiver.pos, sunDir);
-	const uint isCaster = sunDistDiff > 0;
-	
-	float dist = length(receiver.pos - caster.pos + sunDir * sunDistDiff);
-
-	return isCaster  * caster.opacity * min(caster.radius * caster.radius / (receiver.radius * receiver.radius), 1.0f) * 
-		smoothstep(receiver.radius + caster.radius, abs(receiver.radius - caster.radius), dist);
-}
-
-groupshared Particle smem[THREAD_X * THREAD_Y];
-
-[numthreads(THREAD_X, THREAD_Y, 1)]
-void csComputeSelfShadowing(uint tid : SV_GroupIndex)
-{
-	float result = 1.0f;
-	const uint N = THREAD_X * THREAD_Y;
-
-	const Particle p = sbParticles[tid];
-	smem[tid] = p;
-
-	GroupMemoryBarrierWithGroupSync();
-
-	for (uint i = tid + 1; i < N + tid; ++i) {
-		result *= 1.0f - Overlap(smem[i % N], p);
-	}
-
-	sbShadows[tid] = result;
-}
-
-#elif defined SHARED_1
-
-
 
 float Overlap(in Particle caster, in Particle receiver) {
 
@@ -127,4 +54,3 @@ void csComputeSelfShadowing(uint tid : SV_GroupIndex)
 
 	sbShadows[tid] = result;
 }
-#endif
